1489e20030d15b02300bae1db9361bcba92365fa
Xingsheng Qian <xsqian@xsair.lan>
2021-07-31 07:41:10 -0700

    make a change to create a PR

--- project/pipelines/train.py
+++ project/pipelines/train.py
@@ -59,8 +59,3 @@ def kfpipeline():
     # deploy our model as a serverless function
     deploy = funcs["serving"].deploy_step(models={f"{DATASET}_v1": train.outputs['model']},
                                           tag=this_project.params.get('commit', 'v1'))
-
-#     # test out new model server (via REST API calls)
-#     tester = funcs["live_tester"].as_step(name='model-tester',
-#         params={'addr': deploy.outputs['endpoint'], 'model': f"{DATASET}_v1"},
-#         inputs={'table': train.outputs['test_set']})
